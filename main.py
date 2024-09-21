2f0d8d0f5af54d6f9ee23bebc8ae7a3bc2e74679
henriqueGJesus <hj30032006@gmail.com>
2024-09-21 17:27:02 -0300

    atualização de resposta

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, Cookie
+from fastapi import FastAPI, Response, status, Cookie, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from requests.cookies import cookiejar_from_dict
@@ -53,7 +53,7 @@ def handle_redirect(code: str):
 
     # Fazer a requisição para o Instagram para obter o token de acesso
     response_post = requests.post(token_url, data=body_of_request_post)
-
+    response= ""
     if response_post.status_code == 200:
         # Sucesso, obtenha o token de acesso
         access_token_data = response_post.json()
@@ -61,10 +61,33 @@ def handle_redirect(code: str):
         response = Response("Authentication successful", status_code=status.HTTP_302_FOUND)
         response.set_cookie(key="access_token", value=access_token_data['access_token'], httponly=True, secure=True,
                             samesite="Strict")
+    retorno = get_media_ids(response.headers.get('Set-Cookie'))
 
-        return response.headers.get('Set-Cookie')
+    return retorno
 
-    return {"error": "Erro ao trocar o código pelo token", "details": response_post.json()}
+
+# Função para obter os posts de um usuário e encontrar o media_id
+@app.get("/get_media_ids")
+def get_media_ids(access_token: str = Cookie(None)):
+    if not access_token:
+        raise HTTPException(status_code=403, detail="Acesso negado. Token de acesso não fornecido.")
+
+    # URL da API para obter as mídias do usuário
+    media_url = 'https://graph.instagram.com/me/media'
+    params = {
+        'access_token': access_token,
+        'fields': 'id,caption,media_type,media_url'  # Os campos que queremos obter
+    }
+
+    # Fazer a requisição para obter as mídias
+    response = requests.get(media_url, params=params)
+
+    if response.status_code == 200:
+        media_data = response.json()
+        # Retorna os IDs de todas as mídias
+        return media_data
+    else:
+        return {"error": "Erro ao obter mídias", "details": response.json()}
 
 
 # Roda o servidor FastAPI
