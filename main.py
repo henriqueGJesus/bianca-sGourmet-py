40ff57a3802d787144e147e06153d381d421f569
henriqueGJesus <hj30032006@gmail.com>
2024-09-21 17:08:56 -0300

    será que so funciona no vercel?

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def handle_redirect(code: str):
         response.set_cookie(key="access_token", value=access_token_data['access_token'], httponly=True, secure=True,
                             samesite="Strict")
         print(response)
-        return response
+        return response.headers
 
     return {"error": "Erro ao trocar o código pelo token", "details": response_post.json()}
 
