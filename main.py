80b9e210e76a307ce9fb06b66bcce49f49f84b45
henriqueGJesus <hj30032006@gmail.com>
2024-09-28 08:40:03 -0300

    usando nova library para pegar os comentarios

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, Cookie, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-from instascrape import Post
+from instascrape import *
 from requests.cookies import cookiejar_from_dict
 import requests
 
@@ -9,21 +9,23 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://bianca-s-gourmet-356k.vercel.app/*"],  # Permitir a origem do frontend (Vue.js)
+    allow_origins=["http://localhost:5173", "https://bianca-s-gourmet-356k.vercel.app"],
+    # Permitir a origem do frontend (Vue.js)
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos os métodos HTTP (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos os cabeçalhos
 )
-@app.get("/handle_redirect")
-def handle_redirect(code: str):
 
-    post = Post("https://www.instagram.com/p/C-OkdeTJjsZ/")
+
+@app.get("/handle_redirect")
+def handle_redirect():
+    post = Post("https://www.instagram.com/p/C-OkdeTJjsZ/?utm_source=ig_web_copy_link")
 
     post.scrape()
 
     comments = post.get_recent_comments()
 
-    return comments
+    return "deu certo"
 
 
 # Roda o servidor FastAPI
